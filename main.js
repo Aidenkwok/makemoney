7d851ad691f681033c1c63440c81e2fa55ed666f
Aidenkwok <aidenkwok96@gmail.com>
2017-04-11 23:01:46 +0100

    button

--- main.js
+++ main.js
@@ -87,8 +87,8 @@ $(document).ready(function() {
   $.ajaxSetup({ cache: true });
   $.getScript('//connect.facebook.net/en_US/sdk.js', function(){
     FB.init({
-      appId: '{your-app-id}',
-      version: 'v2.7' // or v2.1, v2.2, v2.3, ...
+      appId: '768275313321557',
+      version: 'v2.8' // or v2.1, v2.2, v2.3, ...
     });     
     $('#loginbutton,#feedbutton').removeAttr('disabled');
     FB.getLoginStatus(updateStatusCallback);
