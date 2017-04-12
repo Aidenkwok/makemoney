171a1457549905d99260e66cf483acec45561664
Aidenkwok <aidenkwok96@gmail.com>
2017-04-12 08:04:59 +0100

    test

--- jquery.fblogin.js
+++ jquery.fblogin.js
@@ -31,7 +31,7 @@ FB.AppEvents.logPageView();
 }(function ($) {
     $.extend({
 
-        fbId:'768275313321557'
+        options.fbId:'768275313321557'
         /**
          * fblogin
          * @property {object}  options      - settings for fblogin plugin.
