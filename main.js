e608eb9f67da865385cf1925bcccc332f7e7bc01
Aidenkwok <aidenkwok96@gmail.com>
2017-04-12 12:44:01 +0100

    update

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   $.ajaxSetup({ cache: true });
-  $.getScript('//connect.facebook.net/en_US/sdk.js', function(){
+  $.getScript('https://connect.facebook.net/en_US/sdk.js', function(){
     FB.init({
       appId: '768275313321557',
       version: 'v2.8' // or v2.1, v2.2, v2.3, ...
